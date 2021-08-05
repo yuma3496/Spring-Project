05987cea12801b2026da38659888c51ecadc8f8e
yuma3496 <yuma.ina@gmail.com>
2021-08-05 21:23:49 +0900

    Added Validations

--- src/main/java/net/myprocon/myprocon/form/SignupForm.java
+++ src/main/java/net/myprocon/myprocon/form/SignupForm.java
@@ -1,19 +1,35 @@
 package net.myprocon.myprocon.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
 public class SignupForm {
+
+    @NotBlank
+    @Email
     private String userId;
+
+    @NotBlank
+    @Length(min = 4, max = 100)
+    @Pattern(regexp = "[a-zA-Z0-0]+$")
     private String password;
+
+    @NotBlank
     private String userName;
 
     @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @NotNull
     private Date birthday;
 
+    @Min(20)
+    @Max(100)
     private Integer age;
+
+    @NotNull
     private Integer gender;
 }
