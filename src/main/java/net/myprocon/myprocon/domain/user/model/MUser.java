62f51d6343c4c844d67f699d3adfc093698777be
yuma3496 <yuma.ina@gmail.com>
2021-08-08 16:57:41 +0900

    updated user entity class

--- src/main/java/net/myprocon/myprocon/domain/user/model/MUser.java
+++ src/main/java/net/myprocon/myprocon/domain/user/model/MUser.java
@@ -1,4 +1,16 @@
 package net.myprocon.myprocon.domain.user.model;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class MUser {
+    private String userId;
+    private String password;
+    private Date birthday;
+    private Integer age;
+    private Integer gender;
+    private Integer departmentId;
+    private String role;
 }
