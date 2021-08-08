8fc669fde1c72b96c5a6b0be59eaf7aa97ad1c3b
yuma3496 <yuma.ina@gmail.com>
2021-08-08 17:19:04 +0900

    Updated JavaConfig

--- src/main/java/net/myprocon/myprocon/config/JavaConfig.java
+++ src/main/java/net/myprocon/myprocon/config/JavaConfig.java
@@ -1,4 +1,15 @@
 package net.myprocon.myprocon.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class JavaConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 }
