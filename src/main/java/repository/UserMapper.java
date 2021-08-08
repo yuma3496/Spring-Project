5b8c6ae0fa547ea33a8fbd37a3e7249a3fd83854
yuma3496 <yuma.ina@gmail.com>
2021-08-08 17:06:14 +0900

    update user interface

--- src/main/java/repository/UserMapper.java
+++ src/main/java/repository/UserMapper.java
@@ -1,4 +1,9 @@
 package repository;
 
+import net.myprocon.myprocon.domain.user.model.MUser;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+    public int insertOne(MUser user);
 }
