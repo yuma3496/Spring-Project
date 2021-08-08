22f99e85a208a9dbccd788ef68106368f772ab67
yuma3496 <yuma.ina@gmail.com>
2021-08-08 17:17:09 +0900

    Updated UserServiceImpl

--- src/main/java/net/myprocon/myprocon/service/UserService.java
+++ src/main/java/net/myprocon/myprocon/service/UserService.java
@@ -1,4 +1,10 @@
 package net.myprocon.myprocon.service;
 
+import net.myprocon.myprocon.domain.user.model.MUser;
+
 public class UserService {
+
+    // User Registration
+    public void signup(MUser user) {
+    }
 }
--- src/main/java/net/myprocon/myprocon/service/impl/UserServiceImpl.java
+++ src/main/java/net/myprocon/myprocon/service/impl/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package net.myprocon.myprocon.service.impl;
 
-public class UserServiceImpl {
+import net.myprocon.myprocon.domain.user.model.MUser;
+import net.myprocon.myprocon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.UserMapper;
+
+public class UserServiceImpl extends UserService {
+
+    @Autowired
+    private UserMapper mapper;
+
+    // User Registration
+    @Override
+    public void signup(MUser user) {
+        user.setDepartmentId(1); //Department
+        user.setRole("ROLE_GENERAL"); //Role
+        mapper.insertOne(user);
+    }
 }
