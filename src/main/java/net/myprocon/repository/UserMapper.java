8aea12cd13b764595c7885cb8e788a445fcc99ef
yuma3496 <yuma.ina@gmail.com>
2021-08-11 21:01:55 +0900

    updated user mapper

--- src/main/java/net/myprocon/repository/UserMapper.java
+++ src/main/java/net/myprocon/repository/UserMapper.java
@@ -3,8 +3,14 @@ package net.myprocon.repository;
 import net.myprocon.domain.user.model.MUser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
+    // User Registration
     public int insertOne(MUser user);
+
+    // User list
+    public List<MUser> findMany();
 
 }
