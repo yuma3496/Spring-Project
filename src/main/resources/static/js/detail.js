6e559d41ad0346625ae226abedcf16de061ad516
yuma3496 <yuma.ina@gmail.com>
2021-08-25 11:17:58 +0900

    Updated ajax in detail.js

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -22,4 +22,48 @@ function updateUser() {
     // Get form data
     var formData = $('#user-detail-form').serializeArray();
 
+    // ajax
+    $.ajax({
+        type : "PUT"
+        cache : false,
+        url : '/user/update',
+        data : formData,
+        dataType : 'json',
+    }).done(function(data)) {
+        // When ajax worked
+        alert('User update');
+        // Redirect to user list
+        window.location.href = '/user/list';
+
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        // When ajax failed
+        alert('Update failed');
+    }).always(function()) {
+    });
+
+    // User Deletion
+    function deleteUser() {
+
+    // get form data
+    var formData = $('#user-detail-form').serializeArray();
+
+    // Ajax
+    $.ajax({
+        type :"DELETE",
+        cache : false,
+        url : '/user/delete',
+        data : formData
+        dataType : 'json',
+    }).done(function(data)) {
+        // When ajax worked
+        alert('User deleted');
+        // Redirect to user list
+        window.location.href = '/user/list';
+    }).fail(function(jqXHR, textStatus, errorThrown)) {
+        // When ajax failed
+        alert('Deletion failed');
+
+        ]).always(function) {
+        // function that is always executed
+        });
 }
